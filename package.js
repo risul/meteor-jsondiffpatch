2b55fb0c4d24036466198492d239b678bed209cb
Risul Islam <risul.islam@regfire.com>
2015-01-29 14:11:50 +0100

    Package published

--- package.js
+++ package.js
@@ -1,17 +1,16 @@
 Package.describe({
-    name: 'risul:chance',
-    summary: 'Meteor package for jsondiffpatch - Diff & patch JavaScript objects: https://github.com/benjamine/jsondiffpatch',
-    version: '1.0.0',
+    name: 'risul:jsondiffpatch',
+    summary: 'Meteor package for jsondiffpatch - Diff & patch JavaScript objects.',
+    version: '1.0.3',
     git: 'https://github.com/risul/meteor-jsondiffpatch'
 });
 
 Npm.depends({
-    jsondiffpatch: '0.1.2.7'
+    jsondiffpatch: '0.1.27'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('METEOR@0.9.2.2');
     api.export('jsondiffpatch');
-    api.addFiles('.npm/package/node_modules/jsondiffpatch/public/build/jsondiffpatch-full.min.js', ['client']);
     api.addFiles('lib/jsondiffpatch.js', 'server');
 });
