6b1024a718a12f570b8122f20289f370e752ee37
root <risul.islam@regfire.com>
2015-12-03 16:08:02 +0100

    Updated to latest

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     name: 'risul:jsondiffpatch',
     summary: 'Meteor package for jsondiffpatch - Diff & patch JavaScript objects.',
-    version: '1.0.4',
+    version: '1.0.5',
     git: 'https://github.com/risul/meteor-jsondiffpatch'
 });
 
 Npm.depends({
-    jsondiffpatch: '0.1.33'
+    jsondiffpatch: '0.1.37'
 });
 
 Package.onUse(function(api) {
